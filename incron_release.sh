676a9a0eedfd07a0a95c51cc9402846e7150afc5
xtity <kousuke.sima@gmail.com>
2015-05-13 21:23:15 +0900

    test drone 5

--- incron_release.sh
+++ incron_release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PHOENIX_APP_VERSION=`cat /usr/local/src/phoenix/${PHOENIX_APP_NAME}/current/${PHOENIX_APP_NAME}/VERSION`
 
-cp /usr/local/src/phoenix/${PHOENIX_APP_NAME}/current/${PHOENIX_APP_NAME}/rel/${PHOENIX_APP_NAME}/releases/${PHOENIX_APP_VERSION} /usr/local/app/phoenix/${PHOENIX_APP_NAME}/rel/${PHOENIX_APP_NAME}/releases/
-cp /usr/local/src/phoenix/${PHOENIX_APP_NAME}/current/${PHOENIX_APP_NAME}/rel/${PHOENIX_APP_NAME}/releases/${PHOENIX_APP_NAME}-${PHOENIX_APP_VERSION}.tar.gz /usr/local/app/phoenix/${PHOENIX_APP_NAME}/rel/${PHOENIX_APP_NAME}/releases/${PHOENIX_APP_VERSION}/${PHOENIX_APP_NAME}.tar.gz
+cp -rp /usr/local/src/phoenix/${PHOENIX_APP_NAME}/current/${PHOENIX_APP_NAME}/rel/${PHOENIX_APP_NAME}/releases/${PHOENIX_APP_VERSION} /usr/local/app/phoenix/${PHOENIX_APP_NAME}/rel/${PHOENIX_APP_NAME}/releases/
+cp /usr/local/src/phoenix/${PHOENIX_APP_NAME}/current/${PHOENIX_APP_NAME}/rel/${PHOENIX_APP_NAME}/${PHOENIX_APP_NAME}-${PHOENIX_APP_VERSION}.tar.gz /usr/local/app/phoenix/${PHOENIX_APP_NAME}/rel/${PHOENIX_APP_NAME}/releases/${PHOENIX_APP_VERSION}/${PHOENIX_APP_NAME}.tar.gz
 
 cd /usr/local/app/phoenix/${PHOENIX_APP_NAME}
 rel/${PHOENIX_APP_NAME}/bin/${PHOENIX_APP_NAME} upgrade $PHOENIX_APP_VERSION
