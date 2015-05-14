6902a3b87a0954db1c5f6437087ca336b701df38
xtity <kousuke.sima@gmail.com>
2015-05-14 23:59:10 +0900

    test drone 34

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DeployPhoenix.Mixfile do
 
   def project do
     [app: :deploy_phoenix,
-     version: "0.0.33",
+     version: "0.0.34",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
